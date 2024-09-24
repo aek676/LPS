599ae2e6ef6e6cfa9ba3a8d0fae151692d3f475c
Aula03 <Aula03@AULAIMACE19.local>
2024-09-24 17:06:22 +0200

    Sesion02 init

---
+++ sesion02/sesion02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sesion02
+//
+//  Created by Aula03 on 24/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sesion02/sesion02/sesion02App.swift
@@ -0,0 +1,17 @@
+//
+//  sesion02App.swift
+//  sesion02
+//
+//  Created by Aula03 on 24/9/24.
+//
+
+import SwiftUI
+
+@main
+struct sesion02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
