4688adf2645ceeb1dbb50d2aeb2460778984b1ce
Aula03 <Aula03@AULAIMACE15.local>
2024-10-22 19:40:12 +0200

    Init sesion05

---
+++ sesion05/GestionMascotas/GestionMascotas/GestionMascotasApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionMascotasApp.swift
+//  GestionMascotas
+//
+//  Created by Aula03 on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GestionMascotasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
